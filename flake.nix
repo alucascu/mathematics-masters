c40b8c14b3dc5af6ddea5e0cd4873a61f4111ff6
Asher Lucas-Cuddeback <alucascu@proton.me>
2026-09-15 16:20:12 -0400

    chore(nix): slim LaTeX build by importing only used packages

--- flake.nix
+++ flake.nix
@@ -1,29 +1,64 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  description = "Asher's NMU Mathematics Masters Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
   outputs = {
     self,
     nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        typst
-        tectonic
-        pandoc
-        typstyle
-        ocrmypdf
-        img2pdf
-        qpdf
-        exiftool
-        typos
-        git-lfs
-        just
-        entr
-        biber
-        texliveFull
-      ];
-    };
-  };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      tex = pkgs.texliveSmall.withPackages (
+        ps:
+          with ps; [
+            amsmath
+            amscls # contains amsthm
+            amsfonts # contains amssymb
+            booktabs
+            units
+            tools # contains multicol
+            lipsum
+            fancyvrb
+            geometry
+            mathtools
+            enumitem
+            fancyhdr
+            xcolor
+            hyperref
+            psnfss
+            graphics # contains graphicx
+            titlesec
+            setspace
+            microtype
+            biblatex
+            subfiles
+          ]
+      );
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [tex pkgs.python3];
+
+        packages = with pkgs; [
+          typst
+          pandoc
+          typstyle
+          just
+          git-lfs
+          typos
+          biber
+
+          ocrmypdf
+          img2pdf
+          qpdf
+          exiftool
+        ];
+
+        TEXINPUTS = "./tex//:";
+      };
+    });
 }
